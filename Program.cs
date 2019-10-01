ea4504536718696e55c5ff162a36a8d13c73e8bf
Alexander <alex.esbj@gmail.com>
2019-10-01 20:25:22 +0200

    Lade till instruktioner

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+/* 
+    Nu ska det gå att välja mellan olika städer i programmet. 
+    Vilka städer som finns eller hur många är inte förutbestämt
+    Användaren ska själv via en meny kunna välja eller lägga till en stad.
+    När användaren lägger till en stad får hen ange namn på staden och temperaturen. 
+    Via menyn kan man sedan kontrollera temperatur för alla städer, 
+    medeltemperaturen samt var det är varmast och var det är kallast. 
+*/
 
 namespace weatherstation_2
 {
